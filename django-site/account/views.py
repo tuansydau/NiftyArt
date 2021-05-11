11e9a41b9d3876a9ebe92445e8ab1c2549a9b4fc
tuansydau <tukauu@gmail.com>
2021-05-11 19:51:10 -0400

    django site

---
+++ django-site/account/views.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+from .models import UserBase
+from .token import account_activation_token
+from .forms import RegistrationForm, UserEditForm
+
+
+def account_register(request):
+
+    # Check to see that user is authenticated before registering
+    # if request.user.is_authenticated:
+    # If they are logged in, bring user to homepage
+    # return redirect('/')
+
+    # Make sure that whoever is registering has posted data
+    if request.method == 'POST':
+        # Put the user data into a variable
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject, message=message)
+            return HttpResponse('registered succesfully and activation sent')
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'account/registration/register.html', {'form': registerForm})
+
+
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, user.DoesNotExist):
+        user = None
+
+    # When the user is a user and the token is valid, set user to active and log them in
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('account:dashboard')
+    else:
+        return render(request, 'account/registration/activation_invalid.html')
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard/dashboard.html')
+
+
+@login_required
+def edit_details(request):
+    # When we POST to this view, we will update the user data
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+
+    return render(request,
+                  'account/dashboard/edit_details.html', {'user_form': user_form})
+
+
+@login_required
+def delete_user(request):
+    # Gets the user
+    user = UserBase.objects.get(user_name=request.user)
+    # Don't delete data incase they got hacked, so this means they won't be able to login
+    user.is_active = False
+    user.save()
+    logout(request)
+    # Show the user the account was deleted
+    return redirect('account:delete_confirm')
